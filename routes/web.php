4b5e3302d9d704415055f3e0af820f922dff3b1f
Mex <mexuk@hotmail.co.uk>
2023-01-13 22:35:32 +0000

    updated fallback routes

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VehicleController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/vehicle/show/', [VehicleController::class, 'showAll']);
-Route::get('/vehicle/show/all', [VehicleController::class, 'showAll']);
+$showAll = [VehicleController::class, 'showAll'];
+Route::get('/', $showAll);
+Route::get('/vehicle/', $showAll);
+Route::get('/vehicle/show/', $showAll);
+Route::get('/vehicle/show/all', $showAll);
 Route::get('/vehicle/show/{id}', [VehicleController::class, 'showOne']);
 
 Route::post('/vehicle/create/', [VehicleController::class, 'createOne']);
