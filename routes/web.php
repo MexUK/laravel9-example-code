795852439f3cc518e9565d164feb5604a5b6469f
Mex <mexuk@hotmail.co.uk>
2023-01-13 23:22:08 +0000

    use http redirect 302 (moved temporarily) for fallback urls

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,19 @@
 <?php
 
 use App\Http\Controllers\VehicleController;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Route;
 
-$showAll = [VehicleController::class, 'showAll'];
-Route::get('/', $showAll);
-Route::get('/vehicle/', $showAll);
-Route::get('/vehicle/show/', $showAll);
-Route::get('/vehicle/show/all', $showAll);
+$redirectToShowAll = function():RedirectResponse
+{
+    return redirect('/vehicle/show/all');
+};
+
+Route::get('/', $redirectToShowAll);
+Route::get('/vehicle/', $redirectToShowAll);
+Route::get('/vehicle/show/', $redirectToShowAll);
+
+Route::get('/vehicle/show/all', [VehicleController::class, 'showAll']);
 Route::get('/vehicle/show/{id}', [VehicleController::class, 'showOne']);
 
 Route::post('/vehicle/create/', [VehicleController::class, 'createOne']);
