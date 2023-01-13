7ee9471bffbbf3e8346c7b5b94090f66a69a1aba
Mex <mexuk@hotmail.co.uk>
2023-01-13 23:55:22 +0000

    changed route redirects to only go up 1 folder level in the URL

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,30 @@ use App\Http\Controllers\VehicleController;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Route;
 
-$redirectToShowAllVehicles = function():RedirectResponse
+Route::get('/', function():RedirectResponse
 {
-    return redirect('/vehicle/show/all');
-};
+    return redirect('/vehicle');
+});
 
-Route::get('/', $redirectToShowAllVehicles);
-
-Route::controller(VehicleController::class)->group(function() use ($redirectToShowAllVehicles)
+Route::fallback(function()
 {
-    Route::get('/vehicle', $redirectToShowAllVehicles);
+    return response()->view('/error/404', [], 404);;
+});
+
+Route::controller(VehicleController::class)->group(function()
+{
+    Route::get('/vehicle', function():RedirectResponse
+    {
+        return redirect('/vehicle/show');
+    });
 
     Route::post('/vehicle/create', 'createOne');
     Route::post('/vehicle/destroy', 'destroyOne');
 
-    Route::get('/vehicle/show', $redirectToShowAllVehicles);
+    Route::get('/vehicle/show', function():RedirectResponse
+    {
+        return redirect('/vehicle/show/all');
+    });
     Route::get('/vehicle/show/all', 'showAll');
     Route::get('/vehicle/show/{id}', 'showOne');
 });
